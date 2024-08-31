c4a72ffd87847e5bdaa6d15472dbd92c074c7def
Philippe Mathieu <philippe.mathieu@univ-lille.fr>
2024-08-31 23:13:34 +0200

    ajout chronometrage et aff donnees brutes

--- calcul_nombre_victoires.py
+++ calcul_nombre_victoires.py
@@ -1,5 +1,9 @@
 from testPlusieurs import *
+import time
 import pandas as pd
+
+
+start_time = time.time()
 
 # Tableau du nombre de victoires sur 10000
 # On laisse des 0 sur la diagonale
@@ -14,12 +18,19 @@ resultats_df = pd.DataFrame(0,index=methodes_rangement, columns=methodes_rangeme
 for i, rang_j1 in enumerate(methodes_rangement):
     for j, rang_j2 in enumerate(methodes_rangement):
         if i < j:  # Exclure les symétriques
-            _,vj1,vj2 = simuler_plusieurs(nb_couleurs=4, nb_valeurs=13, nb_parties=nb_parties, rangement_j1=rang_j1, rangement_j2=rang_j2, trace=False)
+            _,vj1,vj2 = simuler_plusieurs(nb_couleurs=4, nb_valeurs=8, nb_parties=nb_parties, rangement_j1=rang_j1, rangement_j2=rang_j2, trace=False)
             resultats_df.loc[rang_j1, rang_j2] += vj1
             resultats_df.loc[rang_j2, rang_j1] += vj2
 
-df_percent = (resultats_df / nb_parties) * 100
 # Afficher le tableau des résultats avec pandas
+print("Tableau du nombre de victoires (sans symétriques) :\n")
+print(resultats_df)
+print()
 print("Tableau des pourcentages de victoires (sans symétriques) :\n")
-#print(resultats_df)
+df_percent = (resultats_df / nb_parties) * 100
 print(df_percent)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+minutes, seconds = divmod(elapsed_time, 60)  # Convert to minutes and seconds
+print(f"\nTemps d'exécution: {int(minutes)} min {int(seconds)} sec")
